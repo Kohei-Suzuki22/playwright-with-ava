45faa447cc65594478939f40f8117b6ea1ed2e26
kohei <bhkbb0551@gmail.com>
2023-01-02 20:27:46 +0900

    sample test playwright with ava

---
+++ tests/ava-example.test.js
@@ -0,0 +1,20 @@
+const { chromium } = require('playwright')
+const test = require('ava').default
+const browserPromise = chromium.launch({ headless: false })
+
+async function pageMacro(t, callback) {
+  const browser = await browserPromise
+  const page = await browser.newPage()
+
+  try {
+    await callback(t, page)
+  } finally {
+    await page.close()
+  }
+}
+
+
+test('Ava integration with playwright', pageMacro, async (t, page) => {
+  await page.goto('https://www.example.com')
+  t.is(await page.title(), 'Example Domain')
+})
